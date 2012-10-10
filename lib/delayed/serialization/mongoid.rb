dfce3666efd47819b94b3a0c8fbd83447af9267f
Christopher Louis Hauboldt <chris@hauboldt.us>
2012-10-10 14:14:54 -0500

    only put object type and id for db objects

--- lib/delayed/serialization/mongoid.rb
+++ lib/delayed/serialization/mongoid.rb
@@ -8,20 +8,20 @@ if YAML.parser.class.name =~ /syck/i
 
     def self.yaml_new(klass, tag, val)
       begin
-        klass.find(val['attributes']['_id'])
+        klass.find(val['_id'])
       rescue Mongoid::Errors::DocumentNotFound
         raise Delayed::DeserializationError
       end
     end
 
     def to_yaml_properties
-      ['@attributes']
+      ['@id']
     end
   end
 else
   Mongoid::Document.class_eval do
     def encode_with(coder)
-      coder["attributes"] = @attributes
+      coder["attributes"] = {'_id' => id}
       coder.tag = ['!ruby/Mongoid', self.class.name].join(':')
     end
   end
